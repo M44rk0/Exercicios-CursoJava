63b11b12ba3b5724e6fe39d8835fc9cbea9c8ba7
Marco Antonio <marcokjjk@gmail.com>
2023-10-31 20:04:08 -0300

    corrigindo erro de src

--- CursoJava/EX23/CarRental.java
+++ CursoJava/EX23/CarRental.java
@@ -6,4 +6,20 @@ public class CarRental {
 
     private Date start;
     private Date finish;
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getFinish() {
+        return finish;
+    }
+
+    public void setFinish(Date finish) {
+        this.finish = finish;
+    }
 }
--- CursoJava/EX23/Invoice.java
+++ CursoJava/EX23/Invoice.java
@@ -5,4 +5,19 @@ public class Invoice {
     private Double basicPayment;
     private Double tax;
 
+    public Double getBasicPayment() {
+        return basicPayment;
+    }
+
+    public void setBasicPayment(Double basicPayment) {
+        this.basicPayment = basicPayment;
+    }
+
+    public Double getTax() {
+        return tax;
+    }
+
+    public void setTax(Double tax) {
+        this.tax = tax;
+    }
 }
